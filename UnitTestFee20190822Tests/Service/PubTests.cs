54f93d51c48878e4dbe3793da9ae6169d85870ca
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-08-22 18:39:06 +0800

    增加模擬一人收為100元預期結果門票收入100

--- UnitTestFee20190822Tests/Service/PubTests.cs
+++ UnitTestFee20190822Tests/Service/PubTests.cs
@@ -37,5 +37,37 @@ namespace UnitTestFee20190822.Service.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+
+        [TestMethod]
+        public void Test_Income_顧客1男2女_應回傳收費人數1人_模擬每一人收費為100元_預期結果門票收入總數為100_使用RhinoMocks物件()
+        {
+            //arrange
+            ICheckInFee stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
+            Pub target = new Pub(stubCheckInFee);
+
+            stubCheckInFee.Stub(x => x.GetFee(Arg<Customer>.Is.Anything)).Return(100);
+
+            var customers = new List<Customer>
+            {
+                new Customer{ IsMale=true},
+                new Customer{ IsMale=false},
+                new Customer{ IsMale=false},
+            };
+
+            var inComeBeforeCheckIn = target.GetInCome();
+            Assert.AreEqual(0, inComeBeforeCheckIn);
+
+            decimal expectedIncome = 100;
+
+            //act
+            var chargeCustomerCount = target.CheckIn(customers);
+
+            var actualIncome = target.GetInCome();
+
+            //assert
+            Assert.AreEqual(expectedIncome, actualIncome);
+        }
+
     }
 }
