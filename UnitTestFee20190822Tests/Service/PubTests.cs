5d7a1bff80043db13c46aa123f384a333165b53d
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-10-04 18:20:23 +0800

    增加練習使用NSubstitute物件做單元測試

--- UnitTestFee20190822Tests/Service/PubTests.cs
+++ UnitTestFee20190822Tests/Service/PubTests.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using UnitTestFee20190822.Repository;
 using Rhino.Mocks;
+using NSubstitute;
+using Arg = NSubstitute.Arg;
 
 namespace UnitTestFee20190822.Service.Tests
 {
@@ -16,7 +18,7 @@ namespace UnitTestFee20190822.Service.Tests
         [TestMethod()]
         public void CheckInTest_顧客1男2女_應回傳收費人數1人_使用RhinoMocks物件()
         {
-            //arrange
+            //arrange準備受測物件、參數、預期結果
             ICheckInFee stubCheckInFee = MockRepository.GenerateStub<ICheckInFee>();
             Pub target = new Pub(stubCheckInFee);
 
@@ -31,10 +33,10 @@ namespace UnitTestFee20190822.Service.Tests
 
             decimal expected = 1;
 
-            //act
+            //act執行受測方法
             var actual = target.CheckIn(customers);
 
-            //assert
+            //assert驗證執行結果與預測結果是否一致
             Assert.AreEqual(expected, actual);
         }
 
@@ -67,6 +69,59 @@ namespace UnitTestFee20190822.Service.Tests
 
             //assert
             Assert.AreEqual(expectedIncome, actualIncome);
+        }
+
+        [TestMethod()]
+        public void CheckInTest_顧客1男2女_應回傳收費人數1人_使用NSubstitute物件()
+        {
+            //arrange準備受測物件、參數、預期結果
+            ICheckInFee CheckInFee = Substitute.For<ICheckInFee>();
+            Pub pub = new Pub(CheckInFee);
+            CheckInFee.GetFee(Arg.Any<Customer>()).Returns(100);
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer{ IsMale=true},
+                new Customer{ IsMale=false},
+                new Customer{ IsMale=false},
+            };
+
+            decimal expected = 1;
+            //act執行受測方法
+            
+            decimal actual = pub.CheckIn(customers);
+
+            //assert驗證執行結果與預測結果是否一致
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Income_顧客1男2女_應回傳收費人數1人_模擬每一人收費為100元_預期結果門票收入總數為100_使用NSubstitute物件()
+        {
+            //arrange準備受測物件、參數、預期結果
+            ICheckInFee CheckInFee = Substitute.For<ICheckInFee>();
+            Pub pub = new Pub(CheckInFee);
+            CheckInFee.GetFee(Arg.Any<Customer>()).Returns(100);
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer{ IsMale=false},
+                new Customer{ IsMale=true},
+                new Customer{ IsMale=false},
+            };
+
+            var inComeBeforeCheckIn = pub.GetInCome();
+            Assert.AreEqual(0, inComeBeforeCheckIn);
+
+            decimal expectedIncome = 100;
+
+            //act執行受測方法
+           
+            decimal chargeCustomerCount = pub.CheckIn(customers);
+            decimal actualIncome = pub.GetInCome();
+
+            //assert驗證執行結果與預測結果是否一致
+            Assert.AreEqual(expectedIncome, actualIncome);
+
         }
 
     }
